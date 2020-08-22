4f2bb7d1b884b110f411e864cff041dafdfb295b
Nobutaka Mitsuhashi <mitsuhashi@biosciencedbc.jp>
2020-08-23 00:15:57 +0900

    Remove getopts from rdf-medgen.sh

--- rdf-medgen.sh
+++ rdf-medgen.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-#
-# -f オプションはダウンロードサイトに新ファイルがなくてもコンバートする
-#
-while getopts f OPT
-do
-  case $OPT in
-     f) OPTION="-f" ;;
-  esac
-done
-
-shift $(($OPTIND - 1))
-
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 "${SCRIPT_DIR}/run.sh" ${OPTION} medgen 
