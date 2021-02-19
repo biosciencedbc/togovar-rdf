6c6355704876e80877c9ee6ff932c506a9db2607
ytokunaga-hi <tokunaga@biosciencedbc.jp>
2021-02-19 10:58:29 +0900

    add check-rdf

---
+++ check-rdf/test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#
+# virtuosoへロードを行いエラーがあれば出力する
+# 1行目 対象ファイル数
+# 2行目 エラー数
+# 3行目以降 エラーファイルと内容
+# -p RDFファイルのパターンを指定、指定がなければロード対象ディレクトリ以下すべてのファイルをロードする
+#
+
+HOST=localhost
+ISQL=isql-v
+PORT=1111
+USER=dba
+PASSWORD=dba
+PATTERN=*
+
+while getopts p: OPT
+do
+  case $OPT in
+    p) PATTERN=$OPTARG ;;
+  esac
+done
+
+# virtuosoの起動
+/virtuoso.sh > /dev/null 2>&1 &
+
+sleep 60
+
+# ロード対象ファイルの追加(ロード対象ディレクトリは/loadに固定) 
+${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="ld_dir('/load', '${PATTERN}', 'check');"
+
+# ロード対象ファイル数を表示
+${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="SELECT COUNT(*) FROM DB.DBA.LOAD_LIST where ll_state = 0;"
+
+# ロードの実行
+${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="rdf_loader_run();" 
+
+# エラー数を表示
+${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="SELECT COUNT(*) FROM DB.DBA.LOAD_LIST WHERE ll_error IS NOT NULL;"
+
+# エラー一覧を表示(無ければ出力なし)
+${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="SELECT * FROM DB.DBA.LOAD_LIST WHERE ll_error IS NOT NULL;"
+
+
+
