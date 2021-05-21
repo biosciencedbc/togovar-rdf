a7c729e409820e3d48909b20516831f7f3545bc9
ytokunaga-hi <tokunaga@biosciencedbc.jp>
2021-05-21 17:13:55 +0900

    modify load virtuoso

--- automated/load_virtuoso-switch.sh
+++ automated/load_virtuoso-switch.sh
@@ -12,17 +12,6 @@ source "${SCRIPT_DIR}/global.conf"
 
 YYYYMMDD=`LANG=C; date +%Y%m%d`
 
-
-# データセットごとの作成日時を読み込む
-DATASETS=("clinvar" "ensembl" "hgnc" "medgen" "pubmed" "pubtator" "nlm-catalog")
-WORK_DIR="${DOCKER_ROOT_DIR}/work"
-declare -A DATASETS_DATE
-
-for dataset in ${DATASETS[@]}; do
-  dataset_date=`cat ${WORK_DIR}/${dataset}_update.txt`
-  DATASETS_DATE["${dataset}"]=${dataset_date}
-done
-
 # virtuosoの初期化
 VIRTUOSO_DIR="${DOCKER_ROOT_DIR}/data"
 
@@ -31,8 +20,6 @@ if [ -d ${VIRTUOSO_DIR} ];then
 fi
 
 mkdir ${VIRTUOSO_DIR}
-
-
 
 # Docker の更新
 DOCKER_DIR="${DOCKER_ROOT_DIR}/virtuoso-switch"
@@ -54,14 +41,6 @@ if [ -s ${DOCKER_LOG_DIR}/${YYYYMMDD}_stderr.log ]; then
   cat ${DOCKER_LOG_DIR}/${YYYYMMDD}_stderr.log >&2
   exit 1
 fi
-
-# ロードした7つの対象データセットの更新日をファイルに出力
-DATE_FILE=${VIRTUOSO_DIR}/dataset_date.tsv
-touch ${DATE_FILE}
-
-for dataset in ${!DATASETS_DATE[@]}; do
-  echo -e "${dataset}\t${DATASETS_DATE[$dataset]}">> ${DATE_FILE}
-done
 
 echo "finish"
 
--- virtuoso-switch/load.sh
+++ virtuoso-switch/load.sh
@@ -36,6 +36,23 @@ sleep 60
 now=`date "+%Y%m%d-%H%M%S"`
 echo "Started load.sh at $now"
 
+# latestの参照先のディレクトリを取得
+CLINVAR_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/clinvar/latest`
+ENSEMBL_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/ensembl/latest`
+HGNC_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/hgnc/latest`
+MEDGEN_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/medgen/latest`
+NLM_CATALOG_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/nlm-catalog/latest`
+PUBMED_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/pubmed/latest`
+PUBTATOR_DATE=`readlink ${LOAD_DATA_BASE}/virtuoso/pubtator/latest`
+DATASETS=("clinvar" "ensembl" "hgnc" "medgen" "pubmed" "pubtator" "nlm-catalog")
+WORK_DIR="${DOCKER_ROOT_DIR}/work"
+declare -A DATASETS_DATE
+
+for dataset in ${DATASETS[@]}; do
+  dataset_date=`readlink ${LOAD_DATA_BASE}/virtuoso/${dataset}/latest`
+  DATASETS_DATE["${dataset}"]=${dataset_date}
+done
+
 # ロード対象の追加
 # add_load_list [ロード対象ファイルのあるディレクトリ] '[対象ファイルのプレフィックス]' '[グラフ名]'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/variant/v3 '*.nt.gz' 'http://togovar.biosciencedbc.jp/variation'
@@ -49,18 +66,18 @@ add_load_list ${LOAD_DATA_BASE}/virtuoso/variant_frequency/v3 '4.7K.*.nt.gz' 'ht
 add_load_list ${LOAD_DATA_BASE}/virtuoso/variant_frequency/v3 '10K.*.nt.gz' 'http://togovar.biosciencedbc.jp/variation/frequency/gem_j_wga'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/variant_frequency/v3 'ExAC.*.ttl.gz' 'http://togovar.biosciencedbc.jp/variation/frequency/exac'
 
-add_load_list ${LOAD_DATA_BASE}/virtuoso/clinvar/latest '*.ttl.gz' 'http://togovar.biosciencedbc.jp/clinvar'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/ensembl/latest  '*.ttl' 'http://togovar.biosciencedbc.jp/ensembl37'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/clinvar/${DATASETS_DATE["clinvar"]} '*.ttl.gz' 'http://togovar.biosciencedbc.jp/clinvar'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/ensembl/${DATASETS_DATE["ensembl"]}  '*.ttl' 'http://togovar.biosciencedbc.jp/ensembl37'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/gwas-catalog/ '*' 'http://togovar.biosciencedbc.jp/gwas-catalog'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/efo/20201110/ 'efo.owl' 'http://togovar.biosciencedbc.jp/efo'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/hco/20180409 '*.ttl' 'http://togovar.biosciencedbc.jp/hco'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/hgnc/latest '*.ttl' 'http://togovar.biosciencedbc.jp/hgnc'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/medgen/latest  '*.ttl' 'http://togovar.biosciencedbc.jp/medgen'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/hgnc/${DATASETS_DATE["hgnc"]} '*.ttl' 'http://togovar.biosciencedbc.jp/hgnc'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/medgen/${DATASETS_DATE["medgen"]}  '*.ttl' 'http://togovar.biosciencedbc.jp/medgen'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/mesh/20201123  '*.nt.gz' 'http://togovar.biosciencedbc.jp/mesh'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/mondo/20201123  '*.owl' 'http://togovar.biosciencedbc.jp/mondo'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/nlm-catalog/latest  '*.ttl' 'http://togovar.biosciencedbc.jp/nlm-catalog'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/pubmed/latest  '*.ttl' 'http://togovar.biosciencedbc.jp/pubmed'
-add_load_list ${LOAD_DATA_BASE}/virtuoso/pubtator/latest  '*.ttl' 'http://togovar.biosciencedbc.jp/pubtator'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/nlm-catalog/${DATASETS_DATE["nlm-catalog"]}  '*.ttl' 'http://togovar.biosciencedbc.jp/nlm-catalog'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/pubmed/${DATASETS_DATE["pubmed"]}  '*.ttl' 'http://togovar.biosciencedbc.jp/pubmed'
+add_load_list ${LOAD_DATA_BASE}/virtuoso/pubtator/${DATASETS_DATE["pubtator"]}  '*.ttl' 'http://togovar.biosciencedbc.jp/pubtator'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/pubcasefinder/20191005/graph-1/data  '*.ttl.gz' 'http://togovar.biosciencedbc.jp/pubcasefinder'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/so/20190301 '*.owl' 'http://togovar.biosciencedbc.jp/so'
 add_load_list ${LOAD_DATA_BASE}/virtuoso/ensembl38/20201012  '*.ttl' 'http://togovar.biosciencedbc.jp/ensembl38'
@@ -97,7 +114,7 @@ if [ ${#ERRORLOG_NOTEXIST[@]} -ge 1 ]; then
 fi	
 
 
-
+# ロードの実行
 pids=()
 for i in $(seq 1 ${PARALLEL}); do
   ${ISQL} -H ${HOST} -S ${PORT} -U ${USER} -P ${PASSWORD} VERBOSE=OFF BANNER=OFF EXEC="rdf_loader_run();" &
@@ -119,6 +136,13 @@ if [ -s /error.log ]; then
   exit 1
 fi
 
+# ロードした7つの対象データセットの更新日をファイルに出力
+DATE_FILE=/data/dataset_date.tsv
+touch ${DATE_FILE}
+
+for dataset in ${!DATASETS_DATE[@]}; do
+  echo -e "${dataset}\t${DATASETS_DATE[$dataset]}">> ${DATE_FILE}
+done
 
 now=`date "+%Y%m%d-%H%M%S"`
 echo "Finished load.sh at $now"
