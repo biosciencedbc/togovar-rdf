d21a803812abc011b135acae739ff26cb568cc85
stahara-hi <tahara@biosciencedbc.jp>
2022-05-25 15:25:58 +0900

    add copy script for registered-access

---
+++ automated/copy_virtuoso_to_dgx1_registered_access.sh
@@ -0,0 +1,39 @@
+#
+# registered-access用virtuosoファイルをdgx1にコピーする
+#
+#
+#
+
+# global.confを読み込む
+SCRIPT_DIR="$(cd $(dirname $0); pwd)"
+source "${SCRIPT_DIR}/global.conf"
+VIRTUOSO_DIR="${ROOT_DIR}/virtuoso-registered-access_load/data"
+
+# コピー元にvirtuoso.db,virtuoso.trxが存在しない場合は異常終了する
+if [ ! -e ${VIRTUOSO_DIR}/virtuoso.db ] || [ ! -e ${VIRTUOSO_DIR}/virtuoso.trx ]; then
+  echo "コピー元のvirtuoso.db または virtuoso.trx が存在しません"
+  exit 1
+fi
+
+ssh dgx1 test -e ${DGX1_VIRTUOSO_REGISTERED_ACCESS_DIR}/job2.lck
+lck_exist=$?
+
+# コピー先にjob2.lckが存在する場合 異常終了
+if [  ${lck_exist} = 0 ]; then
+  echo "dgx1にて同ジョブが実行中です"
+  exit 1
+fi
+
+# ロックファイルの作成
+ssh dgx1 touch ${DGX1_VIRTUOSO_REGISTERED_ACCESS_DIR}/job2.lck
+
+echo "copy file to dgx1"
+
+# dgx1にvirtuosoファイルをコピーする
+scp ${VIRTUOSO_DIR:-/home/togovar/togovar/togovar-dev/togovar-rdf/virtuoso-registered-access_load/data}/* togovar@dgx1:${DGX1_VIRTUOSO_REGISTERED_ACCESS_DIR}
+
+# ロックファイルの削除
+ssh dgx1 rm ${DGX1_VIRTUOSO_REGISTERED_ACCESS_DIR}/job2.lck
+
+echo "finish"
+
